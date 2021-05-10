694017cfefa0cc00db98e85856680b818a65ca91
Yash Gupta <yashg160@gmail.com>
2021-05-11 00:30:33 +0530

    ADD: Route for register

--- web-react/src/App.js
+++ web-react/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "antd/dist/antd.css";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import Home from "./components/Home";
+import Register from "./components/Register";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
           <div />
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/register" component={Register} />
           </Switch>
         </main>
       </div>
