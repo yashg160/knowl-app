de92121df4288a06901e80c2f76ff0aea5e0e9f4
Yash Gupta <yashg160@gmail.com>
2021-05-11 16:52:43 +0530

    ADD: A user home page component

---
+++ web-react/src/components/UserHome/index.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+import { Navbar, Button, Input } from "../Core";
+
+import "./styles/UserHome.module.scss";
+
+function UserHome(props) {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default UserHome;
