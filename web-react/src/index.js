573d55c317e052d37d80c3a7fae89026eee10e08
Yash Gupta <yashg160@gmail.com>
2021-05-14 23:44:39 +0530

    ADD: Auth header and and token

--- web-react/src/index.js
+++ web-react/src/index.js
@@ -1,20 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import registerServiceWorker from './registerServiceWorker'
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_GRAPHQL_URI || "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("TOKEN");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL_URI || '/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
-})
+});
 
 const Main = () => (
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>
-)
+);
 
-ReactDOM.render(<Main />, document.getElementById('root'))
-registerServiceWorker()
+ReactDOM.render(<Main />, document.getElementById("root"));
+registerServiceWorker();
