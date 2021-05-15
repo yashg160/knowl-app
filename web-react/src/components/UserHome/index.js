279f96ec00086839d6f6a0573bea6939aee5ead8
Yash Gupta <yashg160@gmail.com>
2021-05-15 20:31:24 +0530

    ADD: Logic for spaces selection and rediretion

--- web-react/src/components/UserHome/index.js
+++ web-react/src/components/UserHome/index.js
@@ -1,13 +1,40 @@
 import React, { useState, useEffect } from "react";
+import { useMutation, gql, useQuery } from "@apollo/client";
 
-import { Navbar, Button, Input } from "../Core";
+import { Navbar, Button, Input, FullScreenSpinner } from "../Core";
+
+import * as Queries from "../../queries";
 
 import "./styles/UserHome.module.scss";
 
 function UserHome(props) {
+  const userResult = useQuery(Queries.GET_USER);
+  if (userResult.loading) {
+    return <FullScreenSpinner />;
+  }
+
+  if (userResult.data.getUser.status !== "OK") {
+    // User might nit be logged in. Move to sign in screen
+    props.history.push("/signin");
+    return null;
+  }
+
+  // User is logged in and data os available
+  const userData = userResult.data.getUser;
+
+  // First step of profile building is select spaces.
+  if (
+    !userResult.data.getUser.user[0].spaces ||
+    !userResult.data.getUser.user[0].spaces.length === 0
+  ) {
+    props.history.push("/spaces");
+    return null;
+  }
+
   return (
     <>
       <Navbar />
+      <h1>Show popular questions and recommendations etc.</h1>
     </>
   );
 }
