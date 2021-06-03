810a001e13a7e6fdad0e43e44ed7b519720c7861
Yash Gupta <yashg160@gmail.com>
2021-06-03 20:16:10 +0530

    ADD: Route for Ask Question

--- web-react/src/App.js
+++ web-react/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "antd/dist/antd.css";
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+
 import Home from "./components/Home";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
 import UserHome from "./components/UserHome";
 import UserSpaces from "./components/UserSpaces";
+import AskQuestion from "./components/AskQuestion";
+
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
 export default function App() {
   return (
@@ -18,6 +21,7 @@ export default function App() {
             <Route exact path="/signup" component={SignUp} />
             <Route exact path="/signin" component={SignIn} />
             <Route exact path="/home" component={UserHome} />
+            <Route exact path="/askQuestion" component={AskQuestion} />
             <Route exact path="/spaces" component={UserSpaces} />
           </Switch>
         </main>
